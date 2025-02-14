dd4f82e8cf7bf8aac0c3870139805eac4dd122bc
ishaan <ishaan.mohan.sinha@gmail.com>
2025-02-13 22:30:27 -0500

    added memory

--- trainSeq.sh
+++ trainSeq.sh
@@ -3,7 +3,7 @@
 #SBATCH -t 0-12:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p gpu   # Partition to submit to
 #SBATCH --gres=gpu:1
-#SBATCH --mem=50000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=100000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --mail-type=END
