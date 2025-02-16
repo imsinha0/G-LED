d26459467fef52721a32ca8794eb10ffe4319a7e
ishaan <ishaan.mohan.sinha@gmail.com>
2025-02-16 16:17:31 -0500

    diffSeq made

---
+++ diffSeq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -c 1                # Number of cores (-c)
+#SBATCH -t 0-12:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p gpu   # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=300000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --mail-type=END
+#SBATCH --mail-user=imsinha@college.harvard.edu
+
+source ~/.bashrc
+conda activate gledReal
+
+python main_diff_bfs.py
